f39668f31d3f5299b2d21bf15f0d23a30c1b8fe6
hinata1231 <k21kk044@st.kyusan-u.ac.jp>
2023-07-30 14:42:02 +0900

    記事投稿作成画面

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<!DOCTYPE HTML>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
@@ -6,19 +6,18 @@
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
     </head>
-    <body>
-        <h1>title</h1>
-        {{$post->title}}
-        <div class='content'>
-               <div class='content_post'>
+    <body class="antialiased">
+        <h1 class="title">
+            {{ $post->title }}
+        </h1>
+        <div class="content">
+            <div class="content__post">
                 <h3>本文</h3>
-                <p class='body'>{{$post->body}}</p>
-                </div>
-            
+                <p>{{ $post->body }}</p>    
+            </div>
         </div>
-          <div clas='footer'>
-              <a href="/">戻る</a>
-          </div>
+        <div class="footer">
+            <a href="/">戻る</a>
+        </div>
     </body>
 </html>
-        
